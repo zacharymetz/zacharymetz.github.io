e0658a0b28cd5aae85e43dbc3d4ac0be86258fd6
Zachary Metz <zachary.metz@ucalgary.ca>
2021-06-05 20:20:48 -0600

    updated

--- docs/assets/js/lunrsearchengine.js
+++ docs/assets/js/lunrsearchengine.js
@@ -1,37 +1,37 @@
 
 var documents = [{
     "id": 0,
-    "url": "tropicalgalaxy.net/404.html",
+    "url": "/404.html",
     "title": "404",
     "body": "404 Page does not exist!Please use the search bar at the top or visit our homepage! "
     }, {
     "id": 1,
-    "url": "tropicalgalaxy.net/about",
+    "url": "/about",
     "title": "About Tropical Galaxy",
     "body": "Tropical Galaxy is a blog and landing site for various projects exclusively used by me Zach. I am a software engineer from Canada that enjoys solving process automation challenges. Since the beginning of university, I've been coding on the side and experimenting with newly released technologies, inadvertently pulling all-nighters hacking along the way.  In my spare time, I like to develop half-brained business ideas with friends and play with new technologies to learn how they work on a fundamental level. Currently, I'm dabbling in blockchains and corporate tax theory. Questions or bug reports?: Head over to my Github! Support MeObligatory crypto wallets comming soon ;) "
     }, {
     "id": 2,
-    "url": "tropicalgalaxy.net/categories",
+    "url": "/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 3,
-    "url": "tropicalgalaxy.net/blog",
+    "url": "/blog",
     "title": "Tropical Galaxy Net",
     "body": "  Featured Posts:                                                                                                                                                                                                                                                 2D Divisible NFTs                              :               Director Roland Suso Richter’s enigmatic psychological thriller (direct to video/DVD) was based upon screenwriter Michael Cooney’s own play “Point of Death” - a title that. . . :                                                                                                                                                                       Zach                                05 Jun 2021                                                                                                                All Posts:                                                                                                     2D Divisible NFTs              :       Director Roland Suso Richter’s enigmatic psychological thriller (direct to video/DVD) was based upon screenwriter Michael Cooney’s own play “Point of Death” - a title that gave away the film’s entire. . . :                                                                               Zach                05 Jun 2021                                            "
     }, {
     "id": 4,
-    "url": "tropicalgalaxy.net/",
+    "url": "/",
     "title": "Home",
     "body": "                                          Tropical Galaxy                      Technology isn't intrinsically good or evil. It's how it's used, like the death ray. Tropical Galaxy is a place for everything the most certain things in life from tech to taxes.                                                                                                                                                                                                                            Recent Posts:                                                                                                                                                                                                                                                                     2D Divisible NFTs                              :               Director Roland Suso Richter’s enigmatic psychological thriller (direct to video/DVD) was based upon screenwriter Michael Cooney’s own play “Point of Death” - a title that. . . :                                                                                                                                                                       Zach                                05 Jun 2021                                                                                                                        More Posts    "
     }, {
     "id": 5,
-    "url": "tropicalgalaxy.net/robots.txt",
+    "url": "/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 6,
-    "url": "tropicalgalaxy.net/2d-divisible-nfts/",
+    "url": "/2d-divisible-nfts/",
     "title": "2D Divisible NFTs",
     "body": "2021/06/05 - Director Roland Suso Richter’s enigmatic psychological thriller (direct to video/DVD) was based upon screenwriter Michael Cooney’s own play “Point of Death” - a title that gave away the film’s entire plot twist premise. As in many similar films, such as Jacob’s Ladder (1990), Soul Survivors (2001), and The Butterfly Effect (2004), events and people were thoroughly distorted and confused because the protagonist was at the point of death. The tagline was misleading: “When You Don’t Have a Memory, How Can You Remember Who to Trust?” The mind-warping film opened with a hospital patient Simon Cable (Ryan Phillippe) awakening in a hospital with little knowledge (amnesia perhaps?) of what had happened, and why he was there, etc. He was told by attending Dr. Jeremy Newman (Stephen Rea) that it was July 29, 2002 (Simon thought it was the year 2000 - he was confused - he heard a doctor say 20:00 hours!) and that he had died for two minutes from cardiac arrest following the near-fatal accident – but he had been revived (“You’re as good as new”). Dr. Newman: “Simon, this is the 29th of July. The year is 2002. And your wife, whose name is Anna, is waiting outside. ” (The doctor left off four crucial additional words, revealed in the film’s ending. ) (Spoiler: Simon had died and was not resuscitated!). A major clue to everything that truly happened was the scene that played next under the credits - hospital staff failed to bring a patient back to life with a defibrillator after a car accident. Chest compressions failed and there was no pulse. A second major clue was provided by hospital orderly Travis (Stephen Graham): Everybody dies. No mystery there. But why and how everyone dies. Now, there’s a mystery worth solving. Probably the biggest mystery there is. So how do we do spoilers?: 1&lt;span class= spoiler &gt;My hidden paragraph here. &lt;/span&gt;"
     }];
