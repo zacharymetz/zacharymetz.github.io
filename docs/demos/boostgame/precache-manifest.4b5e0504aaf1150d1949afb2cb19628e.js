95e43684d78c280a7216292dcaba8c32bddc88ef
Zachary Metz <zachary.metz@ucalgary.ca>
2021-09-06 13:35:02 -0600

    updated look

---
+++ docs/demos/boostgame/precache-manifest.4b5e0504aaf1150d1949afb2cb19628e.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48d326d19450ad1fbc2781d41ba1f49",
+    "url": "/demos/boostgame/editor.worker.js"
+  },
+  {
+    "revision": "9c49cc83a2102db9fe4eac9313dfd0e7",
+    "url": "/demos/boostgame/index.html"
+  },
+  {
+    "revision": "54b5b56ee87f0bf781bd",
+    "url": "/demos/boostgame/static/css/main.d67416de.chunk.css"
+  },
+  {
+    "revision": "ab13d65781770d41b7b9",
+    "url": "/demos/boostgame/static/js/2.789eb55b.chunk.js"
+  },
+  {
+    "revision": "54b5b56ee87f0bf781bd",
+    "url": "/demos/boostgame/static/js/main.a2067953.chunk.js"
+  },
+  {
+    "revision": "9b02cc2527b9b038f417",
+    "url": "/demos/boostgame/static/js/runtime-main.b41f1e9a.js"
+  },
+  {
+    "revision": "8ebed5e74e881db8c13f7bd978b332dc",
+    "url": "/demos/boostgame/static/media/BAT.8ebed5e7.svg"
+  },
+  {
+    "revision": "d7876d48e80112e766d737346a616707",
+    "url": "/demos/boostgame/static/media/Bitcoin.d7876d48.svg"
+  },
+  {
+    "revision": "d47fb46925e90052a7a1f566812283b4",
+    "url": "/demos/boostgame/static/media/XRP.d47fb469.svg"
+  },
+  {
+    "revision": "db1353b2b96ba1c17e091d0fe0dba3af",
+    "url": "/demos/boostgame/static/media/affiliate.db1353b2.svg"
+  },
+  {
+    "revision": "f306cda7844d52b7a7d4101c6913726e",
+    "url": "/demos/boostgame/static/media/bank.f306cda7.svg"
+  },
+  {
+    "revision": "22a9cab5d1db019c872b32167fbe7345",
+    "url": "/demos/boostgame/static/media/betacoin.22a9cab5.png"
+  },
+  {
+    "revision": "32f9a94e5f4a6553c9a1a631963f1d13",
+    "url": "/demos/boostgame/static/media/betacoin1.32f9a94e.svg"
+  },
+  {
+    "revision": "1b33588cb8d8ab8674314aeb76a9fc38",
+    "url": "/demos/boostgame/static/media/chess-board.1b33588c.svg"
+  },
+  {
+    "revision": "184453072f4d631c731fde9092266d17",
+    "url": "/demos/boostgame/static/media/close.18445307.svg"
+  },
+  {
+    "revision": "651b35e385b78b0555cd0e87b09c6b25",
+    "url": "/demos/boostgame/static/media/coin.651b35e3.svg"
+  },
+  {
+    "revision": "b9d61669701f6ead268a1edc702a295c",
+    "url": "/demos/boostgame/static/media/coinstack.b9d61669.svg"
+  },
+  {
+    "revision": "605642c71463ca8ec99903719c98dcb0",
+    "url": "/demos/boostgame/static/media/dogecoin.605642c7.svg"
+  },
+  {
+    "revision": "81713350c953a254805db5a18317b396",
+    "url": "/demos/boostgame/static/media/eos.81713350.svg"
+  },
+  {
+    "revision": "18708d3e3f54116e2ea639fdd56dd6c5",
+    "url": "/demos/boostgame/static/media/ethereum.18708d3e.svg"
+  },
+  {
+    "revision": "a2b2bb8611d85e25370d6953ebf1ac7c",
+    "url": "/demos/boostgame/static/media/expand.a2b2bb86.svg"
+  },
+  {
+    "revision": "61583dbe807fd087e8f442311cce8cbf",
+    "url": "/demos/boostgame/static/media/fullscreen.61583dbe.svg"
+  },
+  {
+    "revision": "395ace9f47f8c5fabb560da3155dbef5",
+    "url": "/demos/boostgame/static/media/gambling.395ace9f.svg"
+  },
+  {
+    "revision": "19072fe2a7429c24c3a634f7eb15fbe9",
+    "url": "/demos/boostgame/static/media/information-dark.19072fe2.svg"
+  },
+  {
+    "revision": "f99af8e9e241ae2288522f7d7c62089f",
+    "url": "/demos/boostgame/static/media/information.f99af8e9.svg"
+  },
+  {
+    "revision": "804ad7d2d3b7b825f8ddf781403c9ff6",
+    "url": "/demos/boostgame/static/media/isk.804ad7d2.png"
+  },
+  {
+    "revision": "c151518bf73d26f9286495ab4a6ea576",
+    "url": "/demos/boostgame/static/media/litecoin.c151518b.svg"
+  },
+  {
+    "revision": "7c43cdd570e90f9feb83f078bc37c618",
+    "url": "/demos/boostgame/static/media/loading.7c43cdd5.svg"
+  },
+  {
+    "revision": "6b4cb86625058e39591eff6a220d1cc4",
+    "url": "/demos/boostgame/static/media/logout.6b4cb866.svg"
+  },
+  {
+    "revision": "f9caaf1e3ae748a086e6d84093600fe7",
+    "url": "/demos/boostgame/static/media/monero.f9caaf1e.svg"
+  },
+  {
+    "revision": "11e7bd5f1eae908adc5bc0a705747078",
+    "url": "/demos/boostgame/static/media/more.11e7bd5f.svg"
+  },
+  {
+    "revision": "860a10fff0f3b3c6794a21c4852f1ee3",
+    "url": "/demos/boostgame/static/media/price.860a10ff.svg"
+  },
+  {
+    "revision": "791e4bd3fafae098ac03dfc7ab82b2bd",
+    "url": "/demos/boostgame/static/media/rich.791e4bd3.svg"
+  },
+  {
+    "revision": "48b647eb17acf327286b900945b1f9ae",
+    "url": "/demos/boostgame/static/media/send.48b647eb.svg"
+  },
+  {
+    "revision": "f8bbed125e99dfff710e739ee5750ef7",
+    "url": "/demos/boostgame/static/media/spring-swing-rocket.f8bbed12.svg"
+  },
+  {
+    "revision": "1f8c104e496de281af2cbb6b2975edd9",
+    "url": "/demos/boostgame/static/media/user.1f8c104e.svg"
+  }
+]);
